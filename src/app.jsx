6c413304620441b56f8c424adb4f4f51b85320fd
anata <luopc1218@outlook.com>
2021-03-24 10:21:00 +0800

    修改gitignore

--- src/app.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './app';
-
-
-const App = () => {
-  return <div>app</div>;
-};
-
-export default App;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.jsx';
-
-ReactDOM.render(<App />, document.getElementById('root')); //    告诉react渲染什么，在哪儿渲染。这里我们将要渲染一个叫做APP（马上就要创建了）的组件，渲染到我们index.html中的一个div中。
